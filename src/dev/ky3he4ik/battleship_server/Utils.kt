e151eb35812d52bc5d4162a7b40c696bbf7857b5
ky3he4ik <misha10311@yandex.ru>
2021-06-08 05:44:29 +0300

    Fix ip addresses showing

--- src/dev/ky3he4ik/battleship_server/Utils.kt
+++ src/dev/ky3he4ik/battleship_server/Utils.kt
@@ -7,8 +7,12 @@ class Utils {
     companion object {
         fun ipToString(ip: InetSocketAddress): String {
             val sb = StringBuilder()
-            for (b in ip.address.address)
-                sb.append(b).append(".")
+            for (b in ip.address.address) {
+                if (b < 0)
+                    sb.append(b + 256).append(".")
+                else
+                    sb.append(b).append(".")
+            }
             return sb.substring(0, sb.length - 1)
         }
     }
